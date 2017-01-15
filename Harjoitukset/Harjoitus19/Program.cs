944cd1d067f48bfb7e60de3011a7f259f512c3d9
Tuomas <tuomas.luukinen@gmail.com>
2017-01-15 22:49:55 +0200

    Teht 19

--- Harjoitukset/Harjoitus19/Program.cs
+++ Harjoitukset/Harjoitus19/Program.cs
@@ -10,9 +10,52 @@ namespace Harjoitus19
     {
         static void Main(string[] args)
         {
-            string sana = "tietotekniikka";
+            int arvaus = 0;
             const int max = 9;
-
+            char kirjain;
+            bool toisto = true;
+            string sana = "tietotekniikka";
+            char[] piilo = new char[sana.Length];
+            string kopio;
+            for (int i = 0; i < sana.Length; i++)
+            {
+                piilo[i] = '_';
+            }
+            do
+            {
+                kopio = new string(piilo);
+                if (arvaus == max)
+                {
+                    Console.WriteLine("JOUDUIT HIRTEEN!!");
+                    toisto = false;
+                    break;
+                }
+                if (sana == kopio)
+                {
+                    Console.WriteLine("Sana: " + kopio);
+                    Console.WriteLine("Arvaukset: " + arvaus + "/" + max);
+                    Console.WriteLine("Onneksi olkoon, arvasit oikein!");
+                    toisto = false;
+                    break;
+                }
+                Console.WriteLine("Sana: " + kopio);
+                Console.WriteLine("Kaytetyt Arvaukset: " + arvaus + "/" + max);
+                kirjain = char.Parse(Console.ReadLine());
+                if (sana.Contains(kirjain) && !(piilo.Contains(kirjain)))
+                {
+                    for (int i = 0; i < sana.Length; i++)
+                    {
+                        if (sana[i] == kirjain)
+                        {
+                            piilo[i] = kirjain;
+                        }
+                    }
+                }
+                else
+                {
+                    arvaus++;
+                }
+            } while (toisto = true);
         }
     }
 }
