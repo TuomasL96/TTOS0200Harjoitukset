a61d36e8cbfbb76873afbcbfa35ea297b570afd0
k2679 <k2679@ICT>
2017-01-30 13:19:29 +0200

    Did 1,2,3

--- Harjoitukset6/Harjoitukset6/Program.cs
+++ Harjoitukset6/Harjoitukset6/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Harjoitukset6
 {
@@ -10,6 +11,74 @@ namespace Harjoitukset6
     {
         static void Main(string[] args)
         {
+            TextWriter();
+        }
+        static void TextWriter()
+        {
+            string textLine;
+            bool repeat = true;
+            string file = @"d:\temp\testfile.txt";
+            System.IO.StreamWriter outputFile = null;
+
+            while (repeat == true)
+            {
+                Console.Write("Give a text line (0 ends) : ");
+                textLine = Console.ReadLine();
+                if (textLine == "0") { repeat = false; }
+                else
+                {
+                    try
+                    {
+                        outputFile = new System.IO.StreamWriter(file, true);
+                        outputFile.WriteLine(textLine);
+                        outputFile.Close();
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine("Can't open file for writing (UnauthorizedAccessException)");
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        Console.WriteLine("Opened stream is null (ArgumentNullException)");
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("Opened stream is not writable (ArgumentException)");
+                    }
+                    catch (IOException)
+                    {
+                        Console.WriteLine("An IO error happend (IOException)");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Some other exception happend (Exception)");
+                    }
+                    finally
+                    {
+                        if (outputFile != null)
+                        {
+                            outputFile.Close();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("\nContents of " + file + ":");
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(file);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Some exception happened!"); 
+            }
         }
     }
 }
--- Harjoitukset6/Teht3/Program.cs
+++ Harjoitukset6/Teht3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Teht3
 {
@@ -14,9 +15,75 @@ namespace Teht3
         }
         static void MakeAFile()
         {
-            int full = new int();
-            float real = new float();
-
+            string number;
+            int i;
+            double f;
+            bool repeat = true;
+            /* 
+            Voisi varmaan tehdä paljon yksinkertaisemmin/paremmin, nyt ohjelma aukoo ja sulkee tiedostot 
+            joka numeron lisäyksen yhteydessä erikseen.
+            */
+            while (repeat == true)
+            {
+                Console.Write("Give a number: ");
+                number = Console.ReadLine();
+                bool isInt = int.TryParse(number, out i);
+                if (isInt == true)
+                {
+                    System.IO.StreamWriter outputFile = null;
+                    try
+                    {
+                        outputFile = new System.IO.StreamWriter(@"d:\temp\T2Integers.txt", true);
+                        outputFile.WriteLine(i);
+                        outputFile.Close();
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine("File not found (FileNotFoundException)");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Some exception happend (Exception)");
+                    }
+                    finally
+                    {
+                        if (outputFile != null)
+                        {
+                            outputFile.Close();
+                        }
+                    }
+                }
+                else
+                {
+                    bool isDouble = double.TryParse(number, out f);
+                    if (isDouble == true)
+                    {
+                        System.IO.StreamWriter outputFile = null;
+                        try
+                        {
+                            outputFile = new System.IO.StreamWriter(@"d:\temp\T2Doubles.txt", true);
+                            outputFile.WriteLine(f);
+                            outputFile.Close();
+                        }
+                        catch (FileNotFoundException)
+                        {
+                            Console.WriteLine("File not found (FileNotFoundException)");
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Some exception happend (Exception)");
+                        }
+                        finally
+                        {
+                            if (outputFile != null)
+                            {
+                                outputFile.Close();
+                            }
+                        }
+                    }
+                    else { repeat = false; }
+                }
+            }
         }
     }
 }
