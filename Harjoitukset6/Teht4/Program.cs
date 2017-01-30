d008203969601ddb0482f03a32a7e3b938af2f91
k2679 <k2679@ICT>
2017-01-30 13:49:46 +0200

    did 4

--- Harjoitukset6/Teht4/Program.cs
+++ Harjoitukset6/Teht4/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Teht4
 {
@@ -10,6 +13,24 @@ namespace Teht4
     {
         static void Main(string[] args)
         {
+            SaveTvShows();
+        }
+        static void SaveTvShows()
+        {
+            List<TvShow> tvShows = new List<TvShow>();
+            tvShows.Add(new TvShow("Myytinmurtajat", 21, "11:30", "12:30", "pölölölölölölölö"));
+            tvShows.Add(new TvShow("Suomen huutokauppakeisari", 5, "9:30", "10:30", "alalalalalalala"));
+            tvShows.Add(new TvShow("Hieno ohjelma", 5, "15:00", "16:00", "jujujujuujujjujuju"));
+            tvShows.Add(new TvShow("Muumit", 2, "18:00", "19:00", "ummumumumumumumumum"));
+
+            Stream writeStream = new FileStream("TVShows.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(writeStream, tvShows);
+            writeStream.Close();
+
+            Stream readStream = new FileStream("TVShows.bin", FileMode.Open, FileAccess.Read, FileShare.None);
+            TvShow readtvShow = (TvShow)formatter.Deserialize(readStream);
+            Console.WriteLine("TVShow: {0} {1}", TvShow.Name);
         }
     }
 }
---
+++ Harjoitukset6/Teht4/tvShow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht4
+{
+    [Serializable]
+    class TvShows
+    {
+        List<TvShow> tvShows = new List<TvShow>();
+        public string AddShow(TvShow)
+        {
+            return TvShow;
+        }
+    }
+    [Serializable]
+    class TvShow
+    {
+        public string Name { get; set; }
+        public int Channel { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string InfoText { get; set; }
+
+        public TvShow(string name, int channel, string start, string end, string info)
+        {
+            Name = name;
+            Channel = channel;
+            StartTime = start;
+            EndTime = end;
+            InfoText = info;
+        }
+        public override string ToString()
+        {
+            return Name + ", ch: " + Channel + ", starts: " + StartTime + ", ends:" + EndTime + ", INFO: " + InfoText;
+        }
+    }
+}
